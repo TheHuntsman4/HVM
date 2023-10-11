b7aa1a520fd0f00d2022cb4ac7476576bf06e946
TheHuntsman4 <anikethvij464@gmail.com>
2023-10-11 23:40:26 +0530

    <feat>:[Adds the first section of the form]

--- src/app/mainform/page.tsx
+++ src/app/mainform/page.tsx
@@ -1,9 +1,118 @@
-import React from 'react'
+"use client"
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { formSchema } from '@/validators/auth';
+import { useForm } from 'react-hook-form';
+import { useToast } from '@/components/ui/use-toast';
+import { useState } from 'react';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+type Input = z.infer<typeof formSchema>;
+
 
 const page = () => {
+  const { toast } = useToast();
+  const [formStep, setFormStep] = useState(0)
+  const form = useForm<Input>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      name: "",
+      address1: "",
+      address2: "",
+      companyName: '',
+      departmentName: '',
+      facultyDesignation: '',
+      facultyName: '',
+      phoneNumber: '',
+    },
+  });
+
+  function handleSubmit(data: Input) {
+    console.log(data);
+  }
   return (
-    <div className='flex flex-col justify-center items-center'>
-      This is where the main form will be 
+    <div className='h-screen mx-24'>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className='py-12'>
+          <span className=''>Personal Details</span>
+          <div className='bg-[#E9EDFF] h-auto w-full px-2 py-4 space-y-4'>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Full Name" {...field} className='w-1/4' />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="companyName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name of the Company</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Name of the Company" {...field} className='w-1/4' />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className='w-full p-4 grid grid-cols-2 gap-4'>
+              <FormField
+                control={form.control}
+                name="address1"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Addresss Line 1</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Address Line 1"/>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="address2"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Addresss Line 2</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Address Line 2" {...field}/>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
+        </form>
+      </Form>
+      {/* The Submit Button */}
+      <div className='flex flex-col justify-center items-center'>
+        <Button
+          type='button'
+          onClick={() => {
+            console.log(form.getValues());
+          }}
+        >Submit</Button>
+      </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from '@/components/ui/button'
+import { useToast } from '@/components/ui/use-toast'
+import { formSchema } from '@/validators/auth';
+import { zodResolver } from '@hookform/resolvers/zod';
 import Image from 'next/image'
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      This is the Home Page
+      
     </main>
   )
 }

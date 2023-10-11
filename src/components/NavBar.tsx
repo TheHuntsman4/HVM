839022b4b4ff7f4c5500740b3c2b4fed7830480a
TheHuntsman4 <anikethvij464@gmail.com>
2023-10-11 13:29:34 +0530

    <feat>:[Adds validators and some minor UI changes]

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,19 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { buttonVariants } from './ui/button';
-import { HandMetal } from 'lucide-react';
 
 const NavBar = () => {
   return (
     <div className="top-0 w-full z-10 py-2 border-s-amber-500 border-b ">
       <div className='container flex items-center justify-between'>
         <Link href='/'>
-            <HandMetal />
+          <Image src='/Logo.svg'
+            width={250}
+            height={250}
+            alt="Picture of the author" />
         </Link>
         <Link className={buttonVariants()} href='/mainform'>
-            FORM
+          FORM
         </Link>
       </div>
     </div>

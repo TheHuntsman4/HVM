8c7821584a1d89d0c19ad1b9148eebe2fb8a08a3
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-10-14 21:31:28 +0530

    ui: login version 2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,26 +1,25 @@
 "use client";
+import { Input } from '@/components/ui/input';
+import bg from '../../../public/Untitled design.png'
 import React from 'react'
 import {useState} from 'react'
 export default function Page (props : {})  {
-    const [tabstate, settabstate] = useState(true)
     return (
-        <div className='flex items-center justify-center h-[calc(100vh-80px)]'>
-            <div className='flex flex-col bg-[#f58220] w-[25rem] h-[30rem] rounded-3xl'>
-                <div className='flex text-white flex-row w-full h-[16%]'>
-                    <div  onClick = {() => settabstate(true)} className={tabstate ? 'flex bg-[#f58220] rounded-tl-3xl text-2xl items-center z-50 justify-center w-1/2 cursor-pointer' :  'flex bg-[#FFC797] rounded-tl-3xl text-2xl items-center justify-center w-1/2 cursor-pointer'}>                     
-                        User
-                    </div>
-                    <div onClick = {() => settabstate(false)}className={tabstate ? 'flex text-2xl bg-[#FFC797] items-center justify-center w-1/2 rounded-tr-3xl cursor-pointer' : 'flex bg-[#f58220] text-2xl items-center z-50 rounded-tr-3xl justify-center w-1/2 cursor-pointer'} >
+        <div className='bg-back-login h-full'>
+            <div className="flex items-center justify-center  h-full w-full bg-backlogin bg-no-repeat bg-cover bg-center">
+                <div className='flex bg-white/30 h-[30rem] w-[45rem] rounded-3xl shadow-xl  backdrop-blur-sm px-32'>
 
-                        Admin
-                    </div>
-                </div>
-               
-                <div className='h-full z-50 bg-[#f58220] rounded-br-3xl rounded-bl-3xl'>
-                <hr style={{height:"3px",background:"white"}}></hr>
+                        <form className='flex flex-col w-full justify-center items-center'>
+                            <div className='flex flex-col gap-11 w-full mb-10 justify-center items-center'>
+                                <Input className='h-16 text-2xl' placeholder='Username'></Input>
+                                <Input className='h-16 text-2xl'placeholder='Password' type='password'></Input>
+                            </div>
+                            <button className='bg-[#f58220] w-40 text-white h-16 rounded-3xl font-bold text-xl'>LOGIN</button> 
+                        </form>
+    
                 </div>
             </div>
-                        
         </div>
+
     )
 }
